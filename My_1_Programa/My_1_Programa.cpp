880b3f80c0068c66535f16d1372209f382e118d4
joon500 <maliar.kursk@gmail.com>
2021-03-03 12:32:34 +0300

    Добавьте файлы проекта.

---
+++ My_1_Programa/My_1_Programa.cpp
@@ -0,0 +1,49 @@
+﻿// My_1_Programa.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "My_1_Programa.h"
+
+//#define MAX_LOADSTRING 100
+
+
+
+/////INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+
+
+
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+
+    DialogBox(hInstance, MAKEINTRESOURCE(IDD_ABOUTBOX), 0, About);
+  
+
+    // Выполнить инициализацию приложения:
+
+
+}
+
